85fdf7a9a65d594cad61831b00e6250ab13868f0
Frankley Kaiky <kaikym20@gmail.com>
2022-05-03 21:20:54 -0300

    Add Files

---
+++ README.md
@@ -0,0 +1,11 @@
+# CursoPython
+ My progress's repository in "Curso em Video- Python" <br/>
+ Finished in 17/04/2022 <br/>
+ 
+ Udemy/ 100DaysofCode Projects: <br/>
+  * [Hangman](https://github.com/Franky03/Cursos/tree/main/Udemy/Day7/Hangman.py)
+  * [Ceasar Cipher](https://github.com/Franky03/Cursos/blob/main/Udemy/Day8.py)
+  * [BlackJack](https://github.com/Franky03/Cursos/blob/main/Udemy/BlackJack.py)
+  * [HigherLower](https://github.com/Franky03/Cursos/blob/main/Udemy/HigherLower.py)
+  * [Coffee Machine](https://github.com/Franky03/Cursos/blob/main/Udemy/CoffeMachine.py)
+  * ...
---
+++ Udemy/Day16.py
@@ -0,0 +1,35 @@
+# import turtle
+from prettytable import PrettyTable
+table= PrettyTable()
+table.add_column("Pokemon names", ['Pikachu', 'Squirtle', 'Charmander'])
+table.add_column("Type", ['Electric', 'Water', 'Fire'])
+table.align= 'l'
+print(table)
+
+# def posit():
+#     timmy.color("black")
+#     timmy.left(90)
+#     timmy.forward(100)
+#     timmy.left(90)
+#     timmy.forward(10)
+#     timmy.right(90)
+#     timmy.forward(10)
+#     timmy.right(90)
+#     timmy.forward(20)
+
+# timmy= turtle.Turtle()
+# timmy.hideturtle()
+# posit()
+# timmy.color('green')
+# turtle.bgcolor('black')
+# turtle.speed(200)
+# a=0
+# while a<200:
+#     timmy.right(a)
+#     timmy.forward(a *3)
+#     a+=1
+
+# screen= turtle.Screen()
+# screen.canvheight
+# screen.exitonclick()
+
