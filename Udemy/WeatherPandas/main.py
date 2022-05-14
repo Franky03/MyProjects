233014dcd7a20f53f134cc434e08cc34d235f64c
Frankley Kaiky <kaikym20@gmail.com>
2022-05-14 17:23:46 -0300

    Update main.py

--- Udemy/WeatherPandas/main.py
+++ Udemy/WeatherPandas/main.py
@@ -12,3 +12,23 @@ import pandas as pd
 
 data= pd.read_csv("./Udemy/WeatherPandas/weather_data.csv")
 print(data)
+
+data_dict= data.to_dict()
+# print(data_dict)
+# null= data['temp'].isna()
+# print(null)
+media= data['temp'].mean()
+# print(round(media))
+# max= data['temp'].max()
+# print(max)
+# print(data[data.temp== data.temp.max()])
+
+monday= data[data.day=='Monday']
+monday_temp= int(monday.temp)
+new= (((9*monday_temp)+160)/5)
+print(new)
+
+#create a dataframe from scratch
+data_dict= {'students': ['Anya', 'James', 'Angela'], 'scores': [76, 56, 65]}
+df= pd.DataFrame(data_dict)
+df.to_csv('new_data.csv')
